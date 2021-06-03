a06442400900f76d3b649c6df4a638db2a098767
sultonkul070nortoylokov <sultonkul070nortoylokov@gmail.com>
2021-06-03 19:58:24 +0500

    make it better

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ const port = process.env.PORT || 4000;
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 app.listen(port, () => {
   console.log(`Server ${port} da ishga tushdi`);
